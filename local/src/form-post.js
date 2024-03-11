e220fada2625245959015d4aeebb678be12669e6
kkbp27 <kotchakon391@gmail.com>
2024-03-11 11:39:12 +0700

    Create local and server
    Create local and server

---
+++ local/src/form-post.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import react from 'react'
+export default function FormPost(){
+    let[PostedData,setPostedData]=React.useState('')
+    const form = React.useRef()
+
+    const onSubmitForm = (event)=>{
+        event.preventDefault()
+        const formData = new FormData(form.current)
+        const formEnt = Object.fromEntries(formData.entries())
+        fetch('/api/form-post',{
+            method: 'POST',
+            body: JSON.stringify(formEnt),
+            headers:{'Content-Type':'application/json'}
+        })
+    .then(response => response.text())
+    .then(result => setPostedData(result))
+    .catch(err => alert(err))
+    }
+    const inputStyle = {
+    margin: '5px 0',
+    }
+    return(
+        <div style={{margin:'30px'}}>
+            <form ref={form} onSubmit={onSubmitForm}>
+            <div>ติดต่อเรา</div>
+            <input type="text" name="name" size="43" placeholder="ชื่อ" style={inputStyle}/><br/>
+            <input type="email" name="name" size="43" placeholder="อีเมล" style={inputStyle}/><br/>
+            <textarea name="maessage" cols="40" row="4" placeholder="ข้อความ" style={inputStyle}></textarea><br/>
+            <button>ตกลง</button>
+            </form>
+            <div dangerouslySetInnerHTML={{__html: PostedData}}></div>
+        </div>
+    )
+}
